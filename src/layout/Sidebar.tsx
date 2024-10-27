6ff1105bca38085a0b6c65a825c1370bf68c9497
anh-nnd2702 <ducanhnguyennang@gmail.com>
2024-10-27 16:20:31 +0700

    fix: build failed

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,80 +1,65 @@
 import { useState, useEffect, useMemo } from "react";
 import NfAccordion from "components/common/accordion/NfAccordion";
-import { getListCategory, getListArea, getListIngredient } from "services/mealService";
+import { getListCategory, getListArea } from "services/mealService";
 import "./layout.scss";
-import { Autocomplete } from "@mui/material";
 type categoryType = {
   strCategory: string;
 };
 type areaType = {
   strArea: string;
 };
-type ingredientType = {
-  strIngredient: string;
-};
+
 const Sidebar = () => {
   const [categories, setCategories] = useState<categoryType[]>([]);
   const [areas, setAreas] = useState<areaType[]>([]);
-  const [ingredients, setIngredients] = useState<ingredientType[]>([]);
   useEffect(() => {
-    getListCategory().then(res => {
-      setCategories(res.meals)
-    })
-    getListArea().then(res => {
-      setAreas(res.meals)
-    })
-    getListIngredient().then(res => {
-      setIngredients(res.meals)
-    })
-  }, [])
+    getListCategory().then((res) => {
+      setCategories(res.meals);
+    });
+    getListArea().then((res) => {
+      setAreas(res.meals);
+    });
+  }, []);
 
-  const categoriesList = useMemo(() => (
-    <ul>
-      {categories && categories.map((item, index) => (
-        <li key={index}>{item.strCategory}</li>
-      ))}
-    </ul>
-  ), [categories]);
-  const areaList = useMemo(() => (
-    <ul>
-      {areas && areas.map((item, index) => (
-        <li key={index}>{item.strArea}</li>
-      ))}
-    </ul>
-  ), [areas]);
-  const ingredientList = useMemo(() => (
-    <ul>
-      {ingredients && ingredients.map((item, index) => (
-        <li key={index}>{item.strIngredient}</li>
-      ))}
-    </ul>
-  ), [ingredients]);
-  return <div id="sideBar">
-    <div className="sidebar__selector">
-      <NfAccordion 
-        id="categoryAcd" 
-        summary={
-          <h6>Categories</h6>
-        }
-        detail={
-          categoriesList
-        }
-      ></NfAccordion>
+  const categoriesList = useMemo(
+    () => (
+      <ul>
+        {categories &&
+          categories.map((item, index) => (
+            <li key={index}>{item.strCategory}</li>
+          ))}
+      </ul>
+    ),
+    [categories]
+  );
+  const areaList = useMemo(
+    () => (
+      <ul>
+        {areas &&
+          areas.map((item, index) => <li key={index}>{item.strArea}</li>)}
+      </ul>
+    ),
+    [areas]
+  );
+  return (
+    <div id="sideBar">
+      <div className="sidebar__selector">
+        <NfAccordion
+          id="categoryAcd"
+          summary={<h6>Categories</h6>}
+          detail={categoriesList}
+        ></NfAccordion>
+      </div>
+      <div className="sidebar__selector">
+        <NfAccordion
+          id="areaAcd"
+          summary={<h6>Areas</h6>}
+          detail={areaList}
+        ></NfAccordion>
+      </div>
+      <div className="sidebar__selector"></div>
     </div>
-    <div className="sidebar__selector">
-      <NfAccordion 
-        id="areaAcd" 
-        summary={
-          <h6>Areas</h6>
-        }
-        detail={
-          areaList
-        }
-      ></NfAccordion>
-    </div>
-    <div className="sidebar__selector">
-    </div>
-  </div>;
+  );
 };
 
 export default Sidebar;
