bbaf665f884575abf5377d695ab332084f8dd9d8
anh-nnd2702 <ducanhnguyennang@gmail.com>
2024-11-12 22:23:51 +0700

    Add: meal slice

--- src/store/mealSlice.ts
+++ src/store/mealSlice.ts
@@ -1,9 +1,62 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { MealParamType } from "Interfaces/MealIntefaces";
+import { MealType } from "pages/homePage/HomePage";
+import { getMealByFilter } from "services/mealService";
 
-const initialState = {};
+type MealStateType = {
+  meals: MealType[];
+  loading: boolean;
+  filters: MealParamType;
+};
+const initialState = {
+  meals: [],
+  loading: true,
+  filters: {},
+  error: "" as any,
+};
+
+const getListMealAsync = createAsyncThunk(
+  "meal/getListMeal",
+  async (filterParams: MealParamType) => {
+    const res = await getMealByFilter({
+      filterType: filterParams.filterType,
+      category: filterParams.category,
+      area: filterParams.area,
+      ingredient: filterParams.ingredient,
+    })
+      .then((res) => {
+        return res.meals;
+      })
+      .catch((error) => {
+        return error.message || "Failed to fetch meals";
+      });
+    return res;
+  }
+);
 
 const MealSlice = createSlice({
   name: "meal",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    setParams: (state, action) => {
+      state = { ...state, filters: { ...action.payload } };
+    },
+    resetParams: (state, action) => {
+      state = { ...initialState };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getListMealAsync.pending, (state, action) => {
+        state = { ...state, loading: true, error: "" };
+      })
+      .addCase(getListMealAsync.fulfilled, (state, action) => {
+        state = { ...state, loading: false, meals: action.payload, error: "" };
+      })
+      .addCase(getListMealAsync.rejected, (state, action) => {
+        state = { ...state, loading: false, error: action.payload };
+      });
+  },
 });
+
+export default MealSlice;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import MealSlice from "./mealSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    meal: MealSlice.reducer,
+  },
 });
